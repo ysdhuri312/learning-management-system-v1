f04811fbfa339ef6297228de7e385c3acd1ef7b2
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-04-18 23:22:38 +0530

    Setup mongodb database

---
+++ server/src/configs/db.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import mongoose from 'mongoose';
+
+// Connect to the MongoDB database
+const connectDB = async () => {
+  await mongoose.connection.on('connected', () =>
+    console.log('Database Connected'),
+  );
+  await mongoose.connect(process.env.MONGODB_URI);
+};
+
+const disconnectDB = async () => {
+  await mongoose.disconnect;
+};
+
+export { connectDB, disconnectDB };
