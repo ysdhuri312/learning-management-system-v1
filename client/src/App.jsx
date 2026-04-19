8d764d55b98108053206b14ea40380d85a4bcbe7
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-04-19 20:22:31 +0530

    Install and setup react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 /** @format */
 
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Home from './pages/student/Home';
 
 function App() {
-  return <>App</>;
+  return (
+    <div className='text-default min-h-screen bg-white'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
