dd9357c41b6fa238d4f2c9126d3c1ca7a3a99c59
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-04-20 19:30:34 +0530

    Add and setup clerk

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,156 @@
+/** @format */
+
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { useAuth, useUser } from '@clerk/react';
+import humanizeDuration from 'humanize-duration';
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const navigate = useNavigate();
+  const { getToken } = useAuth();
+  const { user } = useUser();
+
+  const [showLogin, setShowLogin] = useState(false);
+  const [isEducator, setIsEducator] = useState(false);
+  const [allCourses, setAllCourses] = useState([]);
+  const [userData, setUserData] = useState(null);
+  const [enrolledCourses, setEnrolledCourses] = useState([]);
+
+  // Fetch All Courses
+  const fetchAllCourses = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + '/api/course/all');
+
+      if (data.success) {
+        setAllCourses(data.courses);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Fetch UserData
+  const fetchUserData = async () => {
+    try {
+      if (user.publicMetadata.role === 'educator') {
+        setIsEducator(true);
+      }
+
+      const token = await getToken();
+
+      const { data } = await axios.get(backendUrl + '/api/user/data', {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (data.success) {
+        setUserData(data.user);
+      } else toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Fetch User Enrolled Courses
+  const fetchUserEnrolledCourses = async () => {
+    const token = await getToken();
+
+    const { data } = await axios.get(
+      backendUrl + '/api/user/enrolled-courses',
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+
+    if (data.success) {
+      setEnrolledCourses(data.enrolledCourses.reverse());
+    } else toast.error(data.message);
+  };
+
+  // Function to Calculate Course Chapter Time
+  const calculateChapterTime = (chapter) => {
+    let time = 0;
+
+    chapter.chapterContent.map((lecture) => (time += lecture.lectureDuration));
+
+    return humanizeDuration(time * 60 * 1000, { units: ['h', 'm'] });
+  };
+
+  // Function to Calculate Course Duration
+  const calculateCourseDuration = (course) => {
+    let time = 0;
+
+    course.courseContent.map((chapter) =>
+      chapter.chapterContent.map(
+        (lecture) => (time += lecture.lectureDuration),
+      ),
+    );
+
+    return humanizeDuration(time * 60 * 1000, { units: ['h', 'm'] });
+  };
+
+  const calculateRating = (course) => {
+    if (course.courseRatings.length === 0) {
+      return 0;
+    }
+
+    let totalRating = 0;
+    course.courseRatings.forEach((rating) => {
+      totalRating += rating.rating;
+    });
+    return Math.floor(totalRating / course.courseRatings.length);
+  };
+
+  const calculateNoOfLectures = (course) => {
+    let totalLectures = 0;
+    course.courseContent.forEach((chapter) => {
+      if (Array.isArray(chapter.chapterContent)) {
+        totalLectures += chapter.chapterContent.length;
+      }
+    });
+    return totalLectures;
+  };
+
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  // Fetch User's Data if User is Logged In
+  useEffect(() => {
+    if (user) {
+      fetchUserData();
+      fetchUserEnrolledCourses();
+    }
+  }, [user]);
+
+  const value = {
+    showLogin,
+    setShowLogin,
+    backendUrl,
+    currency,
+    navigate,
+    userData,
+    setUserData,
+    getToken,
+    allCourses,
+    fetchAllCourses,
+    enrolledCourses,
+    fetchUserEnrolledCourses,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateRating,
+    calculateNoOfLectures,
+    isEducator,
+    setIsEducator,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,24 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+
 import { BrowserRouter } from 'react-router';
+import { AppContextProvider } from './context/AppContext.jsx';
+import { ClerkProvider } from '@clerk/react';
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </ClerkProvider>
   </BrowserRouter>,
 );
