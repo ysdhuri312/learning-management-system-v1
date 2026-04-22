94e684f4f66c7dcf386ef42e8dd6ebdf04b9257a
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-04-22 19:46:13 +0530

    Create get educator courses router and their controller

--- server/src/controllers/educator.controller.js
+++ server/src/controllers/educator.controller.js
@@ -51,3 +51,16 @@ export const addCourse = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// Get Educator Courses
+export const getEducatorCourses = async (req, res) => {
+  try {
+    const educator = req.auth.userId;
+
+    const courses = await Course.find({ educator });
+
+    res.json({ success: true, courses });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
--- server/src/routers/educator.routes.js
+++ server/src/routers/educator.routes.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import {
   addCourse,
+  getEducatorCourses,
   updateRoleToEducator,
 } from '../controllers/educator.controller.js';
 import upload from '../configs/multer.js';
@@ -15,5 +16,8 @@ router.get('/update-role', updateRoleToEducator);
 
 // Add Courses
 router.post('/add-course', upload.single('image'), protectEducator, addCourse);
+
+// Get Educator Courses
+router.get('/courses', protectEducator, getEducatorCourses);
 
 export default router;
