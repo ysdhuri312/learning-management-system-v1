d2f00c2ebceef88264f279530c8233af31618ca4
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-04-23 20:15:24 +0530

    Create courses list page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,20 +8,22 @@ import { ToastContainer } from 'react-toastify';
 import Home from './pages/student/Home';
 import Navbar from './components/student/Navbar';
 import CourseDetails from './pages/student/CourseDetails';
+import CoursesList from './pages/student/CoursesList';
 
 function App() {
   const isEducatorRoute = useMatch('/educator/*');
 
   return (
     <div className='text-default min-h-screen bg-white'>
-      <ToastContainer>
-        {/* Render Student Navbar only if not on educator routes */}
-        {!isEducatorRoute && <Navbar />}
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/course/:id' element={<CourseDetails />} />
-        </Routes>
-      </ToastContainer>
+      <ToastContainer />
+      {/* Render Student Navbar only if not on educator routes */}
+      {!isEducatorRoute && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/course/:id' element={<CourseDetails />} />
+        <Route path='/course-list' element={<CoursesList />} />
+        <Route path='/course-list/:input' element={<CoursesList />} />
+      </Routes>
     </div>
   );
 }
