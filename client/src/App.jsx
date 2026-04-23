c8ce5a85ae6bb68a37dff3f0ba1136d8f465aced
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-04-23 19:51:59 +0530

    Create course details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,27 @@
 /** @format */
 
 import { Route, Routes, useMatch } from 'react-router';
+import 'quill/dist/quill.snow.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 import Home from './pages/student/Home';
 import Navbar from './components/student/Navbar';
+import CourseDetails from './pages/student/CourseDetails';
 
 function App() {
   const isEducatorRoute = useMatch('/educator/*');
 
   return (
     <div className='text-default min-h-screen bg-white'>
-      {/* Render Student Navbar only if not on educator routes */}
-      {!isEducatorRoute && <Navbar />}
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <ToastContainer>
+        {/* Render Student Navbar only if not on educator routes */}
+        {!isEducatorRoute && <Navbar />}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/course/:id' element={<CourseDetails />} />
+        </Routes>
+      </ToastContainer>
     </div>
   );
 }
