ef64a9b1da7410cdb2a08812874627b6cc10396d
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-04-24 19:06:23 +0530

    Navbar for educator page

---
+++ client/src/components/educator/Navbar.jsx
@@ -0,0 +1,31 @@
+/** @format */
+
+import { useContext } from 'react';
+import { assets } from '../../assets/assets';
+import { Link } from 'react-router';
+import { AppContext } from '../../context/AppContext';
+import { UserButton, useUser } from '@clerk/react';
+
+const Navbar = ({ bgColor }) => {
+  const { isEducator } = useContext(AppContext);
+  const { user } = useUser();
+
+  return (
+    isEducator &&
+    user && (
+      <div
+        className={`flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3 ${bgColor}`}
+      >
+        <Link to='/'>
+          <img src={assets.logo} alt='Logo' className='w-28 lg:w-32' />
+        </Link>
+        <div className='flex items-center gap-5 text-gray-500 relative'>
+          <p>Hi! {user.fullName}</p>
+          <UserButton />
+        </div>
+      </div>
+    )
+  );
+};
+
+export default Navbar;
