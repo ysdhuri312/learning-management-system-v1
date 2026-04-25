62420ace028aaefae5adc8b083e7b9d8cb1b73fb
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-04-25 11:29:52 +0530

    Create my-courses page for educator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import Player from './pages/student/Player';
 import Educator from './pages/educator/Educator';
 import Dashboard from './pages/educator/Dashboard';
 import AddCourse from './pages/educator/AddCourse';
+import MyCourses from './pages/educator/MyCourses';
 
 function App() {
   const isEducatorRoute = useMatch('/educator/*');
@@ -35,6 +36,7 @@ function App() {
         <Route path='/educator' element={<Educator />}>
           <Route path='/educator' element={<Dashboard />} />
           <Route path='add-course' element={<AddCourse />} />
+          <Route path='my-courses' element={<MyCourses />} />
         </Route>
       </Routes>
     </div>
