9047cecdb76e93c9055393ddfe05308f45cd9be1
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-04-30 12:17:23 +0530

    Update db uri

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'lms',
+      dbName: 'lms-v1',
     });
 
     console.log(`✅ Database Connected: ${conn.connection.host}`);
